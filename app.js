f80d166beacd3103e147dee27daf08ca717a2933
Richnei <rich.nei@hotmail.com>
2022-01-27 16:57:43 -0300

    dates

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const userRouter = require('./routes/userRouter')
+const userRouter = require("./routes/userRouter");
+const mongoose = require("mongoose");
 
-app.use('/user', userRouter);
+mongoose.connect(
+  process.env.MONGO_CONNECTION_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (error) => {
+    if (error) console.log(error);
+    else console.log("Mongo Connected");
+  }
+);
+
+app.use("/user", userRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("Server Running");
