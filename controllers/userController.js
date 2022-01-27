f055471bcbd6ab043b807ea3e6522207d85d5eda
Richnei <rich.nei@hotmail.com>
2022-01-27 16:00:18 -0300

    control

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,12 @@
+const userController = {
+  register: function (req, res) {
+    console.log("register");
+    res.send("register");
+  },
+  login: function login(req, res) {
+    console.log("login");
+    res.send("login");
+  },
+};
 
-
-function register(req,res) {
-    
-}
+module.exports = userController;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const userController = require("../controllers/userController");
 
-router.post("/register", (req, res) => {
-  console.log("register");
-  res.send("register");
-});
-router.post("/login", (req, res) => {
-  console.log("login");
-  res.send("login");
-});
+router.post("/register", userController.register);
+router.post("/login", userController.login);
 
 module.exports = router;
